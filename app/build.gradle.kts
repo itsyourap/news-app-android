69d52300b5efc0dc451dae73eadad2d5f4dde086
Ankan Pal <itsyourap@gmail.com>
2024-01-23 10:16:45 +0530

    Initialize dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
 }
 
 android {
@@ -50,15 +52,67 @@ android {
 }
 
 dependencies {
-
+    // Android Core KTX
     implementation("androidx.core:core-ktx:1.12.0")
+
+    // Android Lifecycle KTX
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    // Activity Compose
     implementation("androidx.activity:activity-compose:1.8.2")
+
+    // Compose
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+
+    // Material3
     implementation("androidx.compose.material3:material3")
+
+    // Splash Screen API
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    // Compose Navigation
+    val navComposeVersion = "2.7.6"
+    implementation("androidx.navigation:navigation-compose:$navComposeVersion")
+
+    // Dagger Hilt
+    val daggerHiltVersion = "2.50"
+    implementation("com.google.dagger:hilt-android:$daggerHiltVersion")
+    ksp("com.google.dagger:hilt-compiler:$daggerHiltVersion")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    // Retrofit
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
+    // Datastore
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    // Compose Foundation
+    implementation("androidx.compose.foundation:foundation:1.5.4")
+
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
+
+    // Paging 3
+    val pagingVersion = "3.2.1"
+    implementation("androidx.paging:paging-runtime-ktx:$pagingVersion")
+    implementation("androidx.paging:paging-compose:$pagingVersion")
+
+    // Room
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+
+    // Test Dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
