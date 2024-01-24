a15dc28e5a0a40c9c7524a0b41fafd84ca0c6324
Ankan Pal <itsyourap@gmail.com>
2024-01-24 18:57:08 +0530

    Create Page Indicator

---
+++ app/src/main/java/dev/itsyourap/newsapp/presentation/common/PageIndicator.kt
@@ -0,0 +1,35 @@
+package dev.itsyourap.newsapp.presentation.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import dev.itsyourap.newsapp.presentation.onboarding.ui.Dimens.IndicatorSize
+
+@Composable
+fun PageIndicator(
+    modifier: Modifier = Modifier,
+    pageSize: Int,
+    selectedPage: Int,
+    selectedColor: Color = MaterialTheme.colorScheme.primary,
+    unselectedColor: Color = Color.DarkGray
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        repeat(pageSize) { page ->
+            Box(
+                modifier = Modifier.size(IndicatorSize).clip(CircleShape)
+                    .background(color = if (page == selectedPage) selectedColor else unselectedColor)
+            )
+        }
+    }
+}
